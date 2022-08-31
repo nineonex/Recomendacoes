3f440cdb99385c0d0f0ba81b2601c11c3d6772c3
RickMartian <rikmarciano@outlook.com.br>
2022-08-31 14:46:28 -0300

    feat: change carousel items movement and some minor issues

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -13,11 +13,11 @@ export function Carousel() {
 
   const handleButtonRight = (e) => {
     e.preventDefault()
-    carousel.current.scrollLeft += carousel.current.offsetWidth
+    carousel.current.scrollLeft += carousel.current.firstChild.clientWidth
   }
   const handleButtonLeft = (e) => {
     e.preventDefault()
-    carousel.current.scrollLeft -= carousel.current.offsetWidth
+    carousel.current.scrollLeft -= carousel.current.firstChild.clientWidth
   }
   if (!data || !data.length) return null
   return (
@@ -35,14 +35,14 @@ export function Carousel() {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke-width='1.5'
+          strokeWidth='1.5'
           stroke='currentColor'
           width='40px'
           height='40px'
         >
           <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeLinecap='round'
+            strokeLinejoin='round'
             d='M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5'
           />
         </svg>
@@ -76,14 +76,14 @@ export function Carousel() {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke-width='1.5'
+          strokeWidth='1.5'
           stroke='currentColor'
           width='40px'
           height='40px'
         >
           <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeLinecap='round'
+            strokeLinejoin='round'
             d='M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5'
           />
         </svg>
